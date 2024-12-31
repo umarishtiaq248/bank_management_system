11c68b1ad68f92bf1aebf3deea2f0c5e575c43ad
umarishtiaq248 <umarishtiaq63@gmail.com>
2024-12-31 12:05:24 +0500

    switch to lower case and security key secure

--- bank_management_system/asgi.py
+++ bank_management_system/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Bank_Management_System.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bank_management_system.settings")
 
 application = get_asgi_application()
