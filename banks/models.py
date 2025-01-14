f3fc2b33100fd3b8d5c03cd515be714520342783
umarishtiaq248 <umarishtiaq63@gmail.com>
2025-01-14 18:30:20 +0500

    URL is set_up,View is attached with url,Bank and Account Model is created in Bank app

--- banks/models.py
+++ banks/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
 # Create your models here.
+class Bank(models.Model):
+    bank_name=models.CharField(max_length=128)
+    branch_name = models.CharField(max_length=128)
+    is_islamic = models.BooleanField(null=True)
+
+    def __str__(self):
+        return self.bank_name
+
+
+class Account(models.Model):
+    user_name=models.CharField(max_length=128)
+    balance=models.FloatField()
+    bank=models.ForeignKey(Bank,on_delete=models.CASCADE,related_name="accounts")
+
+    def __str__(self):
+        return f"{self.user_name}"
